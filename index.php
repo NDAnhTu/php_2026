f19c38c2c337c637493fde907504b4c7b8120a30
ndanhtu <nguyendoanhtu14@gmail.com>
2026-03-01 19:35:54 +0700

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require "functions.php";
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if ($uri === "/") {
+    require "controllers/index.php";
+} elseif ($uri === "/about") {
+    require "controllers/about.php";
+} elseif ($uri === "/contact") {
+    require "controllers/contact.php";
+} else {
+    http_response_code(404);
+    echo "404 Not Found";
+}
