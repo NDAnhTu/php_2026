72b9af4502b10c03845c1e87cecd1ebee421bd6a
ndanhtu <nguyendoanhtu14@gmail.com>
2026-03-02 10:30:06 +0700

    PDO First Stepss

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 require "functions.php";
-require "router.php";
+// require "router.php";
+
+$dsn = "mysql:host=127.0.0.1;port=3306;dbname=myapp;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root');
+
+$stament = $pdo->prepare("SELECT * FROM posts");
+$stament->execute();
+$posts = $stament->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
