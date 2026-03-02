ff0eb25e88be1f0ec2e91aed05f5b7def7bdac06
ndanhtu <nguyendoanhtu14@gmail.com>
2026-03-02 11:49:11 +0700

    Environments and Configuration Flexibility

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ require "functions.php";
 require "database.php";
 // require "router.php";
 
+$config = require "config.php";
 
-
-$db = new Database();
+$db = new Database($config['database']);
 $posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
 dd($posts);
