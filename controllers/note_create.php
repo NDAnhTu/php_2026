760af0d50ceaa6f6acf2cf486b8bd7333bfad380
ndanhtu <nguyendoanhtu14@gmail.com>
2026-03-03 09:17:30 +0700

    Intro to Form Validations

--- controllers/note_create.php
+++ controllers/note_create.php
@@ -4,7 +4,18 @@ $config = require "config.php";
 $db = new Database($config['database']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (!empty($_POST['body'])) {
+    $errors = [];
+    $maxLength = 1000;
+
+    if (empty($_POST['body'])) {
+        $errors['body'] = "Body is required";
+    }
+
+    if (strlen($_POST['body']) > $maxLength) {
+        $errors['body'] = "Body must be less than 1000 characters";
+    }
+
+    if (empty($errors)) {
         $db->query("INSERT INTO notes (body, user_id) VALUES (?, ?)", [$_POST['body'], 1]);
         header("Location: /notes");
         exit;
--- views/note_create.view.php
+++ views/note_create.view.php
@@ -8,9 +8,13 @@
         <form method="POST">
             <div class="mb-4">
                 <label for="body" class="block text-gray-700 font-bold mb-2">Note Body:</label>
-                <textarea name="body" id="body" rows="4" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+                <textarea name="body" id="body" rows="4" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required placeholder="This is a sample note body...">
+                    <?= isset($_POST['body']) ? htmlspecialchars($_POST['body']) : '' ?>
+                </textarea>
             </div>
-
+            <?php if (isset($errors['body'])): ?>
+                <p class="text-red-500 text-xl italic mb-4"><?php echo $errors['body']; ?></p>
+            <?php endif; ?>
             <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Create Note</button>
         </form>
     </div>
