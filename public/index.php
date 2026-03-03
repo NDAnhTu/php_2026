ae1fd300972f99c410d14958c87d958b02d9b94b
ndanhtu <nguyendoanhtu14@gmail.com>
2026-03-03 14:47:26 +0700

    Namespacing: What, Why, How?

--- public/index.php
+++ public/index.php
@@ -2,12 +2,11 @@
 
 const BASE_PATH = __DIR__ . '/../';
 
-require BASE_PATH . "functions.php";
+require BASE_PATH . "core/functions.php";
 
 spl_autoload_register(function ($class) {
-    // require base_path("database.php");
-    // require base_path("response.php");
-    require base_path("core/" . strtolower($class) . ".php");
+    // Core\Database
+    require base_path(str_replace('\\', DIRECTORY_SEPARATOR, $class) . ".php");
 });
 
-require base_path("router.php");
+require base_path("core/router.php");
