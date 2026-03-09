2df62918088cb69fc9500d1572f22ead0eaf015f
ndanhtu <nguyendoanhtu14@gmail.com>
2026-03-09 15:47:17 +0700

    Composer and Free Autoloading

--- public/index.php
+++ public/index.php
@@ -9,10 +9,12 @@ const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . "core/functions.php";
 
-spl_autoload_register(function ($class) {
-    // Core\Database
-    require base_path(str_replace('\\', DIRECTORY_SEPARATOR, $class) . ".php");
-});
+require BASE_PATH . "vendor/autoload.php";
+
+// spl_autoload_register(function ($class) {
+//     // Core\Database
+//     require base_path(str_replace('\\', DIRECTORY_SEPARATOR, $class) . ".php");
+// });
 
 $router = new Core\Router();
 
